ba5e06bbcaf3e28b649b613d420dbaf9410fbc19
Ken <ourpro@naver.com>
2016-09-05 17:25:11 +0900

    Add styles to weather page

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -15,7 +15,7 @@ var React = require('react');
 var WeatherMessage = ({temp, location}) => {
   return (
       <div>
-        <h3>It is {temp} in {location}.</h3>
+        <h3 className="text-center">지금 현재 {location} 온도는 {temp}도입니다.</h3>
       </div>
   );
 }
